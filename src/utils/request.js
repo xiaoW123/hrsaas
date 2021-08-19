eead6b882396d170a9054306356904d8955378ef
xiaoW123 <wl1223171@outlook.com>
2021-08-19 10:40:57 +0800

    update

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,8 @@ import axios from 'axios'
 const service = axios.create({
   // 这里的process.env就是Nodejs提供的一个API，它返回一个包含用户环境信息的对象
   // VUE_APP_BASE_API是.env.production(生产)和.env.development(开发)的对应的接口环境变量
+  // 当我们使用 npm run dev时调用的是.env.development(开发)的对应的接口环境变量
+  // 当我们使用 npm run buil时调用的是.env.production(生产)的对应的接口环境变量
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000 // 定义5秒超时
 })
